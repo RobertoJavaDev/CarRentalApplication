66c9c8901519eab41e08644617e6e0f37c325fdc
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-28 10:07:21 +0200

    added new interface BodyStyleRepository

---
+++ src/main/java/pl/robertojavadev/carrentalapp/bodystyle/domain/repository/BodyStyleRepository.java
@@ -0,0 +1,8 @@
+package pl.robertojavadev.carrentalapp.bodystyle.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BodyStyleRepository extends JpaRepository {
+}
