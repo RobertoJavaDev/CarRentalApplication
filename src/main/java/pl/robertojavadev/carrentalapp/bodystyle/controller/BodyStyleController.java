70fdcb094428a056ee25b0f29a920d315bcf8a8a
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-28 07:53:17 +0200

    added BodyStyleController

---
+++ src/main/java/pl/robertojavadev/carrentalapp/bodystyle/controller/BodyStyleController.java
@@ -0,0 +1,43 @@
+package pl.robertojavadev.carrentalapp.bodystyle.controller;
+
+import org.springframework.web.bind.annotation.*;
+import pl.robertojavadev.carrentalapp.bodystyle.domain.model.BodyStyle;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/body-styles")
+public class BodyStyleController {
+
+    private final BodyStyleService bodyStyleService;
+
+    public BodyStyleController(BodyStyleService bodyStyleService) {
+        this.bodyStyleService = bodyStyleService;
+    }
+
+    @GetMapping
+    public List<BodyStyle> getBodyStyles(){
+        return null;
+    }
+
+    @GetMapping("{id}")
+    public BodyStyle getBodyStyle(@PathVariable UUID id){
+        return null;
+    }
+
+    @PostMapping
+    public BodyStyle createBodyStyle(@RequestBody BodyStyle bodyStyle){
+        return null;
+    }
+
+    @PutMapping("{id}")
+    public BodyStyle updateBodyStyle(@PathVariable UUID id, @RequestBody BodyStyle bodyStyle){
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteBodyStyle(@PathVariable UUID id){
+
+    }
+}
