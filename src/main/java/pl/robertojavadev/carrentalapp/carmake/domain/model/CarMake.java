a4aa9e90410c8f988afd57d1400909f49e5033b8
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-28 08:12:43 +0200

    implemented CarMake class

--- src/main/java/pl/robertojavadev/carrentalapp/carmake/domain/model/CarMake.java
+++ src/main/java/pl/robertojavadev/carrentalapp/carmake/domain/model/CarMake.java
@@ -1,4 +1,41 @@
 package pl.robertojavadev.carrentalapp.carmake.domain.model;
 
+import java.util.UUID;
+
 public class CarMake {
+
+    private UUID id;
+    private String name;
+
+    public CarMake(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CarMake() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CarMake{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
