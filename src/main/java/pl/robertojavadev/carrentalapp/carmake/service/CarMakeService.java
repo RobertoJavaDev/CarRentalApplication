1df207019c13b1baf520e252765c89a0cde897d4
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-28 08:30:46 +0200

    implemented CarMakeService class

--- src/main/java/pl/robertojavadev/carrentalapp/carmake/service/CarMakeService.java
+++ src/main/java/pl/robertojavadev/carrentalapp/carmake/service/CarMakeService.java
@@ -1,4 +1,30 @@
 package pl.robertojavadev.carrentalapp.carmake.service;
 
+import org.springframework.stereotype.Service;
+import pl.robertojavadev.carrentalapp.carmake.domain.model.CarMake;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class CarMakeService {
+
+    public List<CarMake> getCarMakes(){
+        return null;
+    }
+
+    public CarMake getCarMake(UUID id){
+        return null;
+    }
+
+    public CarMake createCarMake(CarMake carMakeRequest){
+        return null;
+    }
+
+    public CarMake updateCarMake(UUID id, CarMake carMakeRequest){
+        return null;
+    }
+
+    public void deleteCarMake(UUID id){
+    }
 }
