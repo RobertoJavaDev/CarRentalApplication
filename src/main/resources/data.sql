05c95c975c5786d2c14f58d72dcc3b0519fcb516
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-28 09:57:11 +0200

    added table body_styles to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into body_styles (id, name ) values
+                                        (gen_random_uuid(),'Micro'),
+                                        (gen_random_uuid(),'Hatchback'),
+                                        (gen_random_uuid(),'Sedan'),
+                                        (gen_random_uuid(),'SUV'),
+                                        (gen_random_uuid(),'Luxury'),
+                                        (gen_random_uuid(),'Coupe'),
+                                        (gen_random_uuid(),'Sport car'),
+                                        (gen_random_uuid(),'Sport car');
+
