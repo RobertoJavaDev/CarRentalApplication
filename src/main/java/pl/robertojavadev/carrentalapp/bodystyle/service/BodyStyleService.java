46ae7ba68b9ab44d70fc879874d9fcc6566d9290
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-28 10:09:21 +0200

    added new repository object to BodyStyleService

--- src/main/java/pl/robertojavadev/carrentalapp/bodystyle/service/BodyStyleService.java
+++ src/main/java/pl/robertojavadev/carrentalapp/bodystyle/service/BodyStyleService.java
@@ -2,12 +2,19 @@ package pl.robertojavadev.carrentalapp.bodystyle.service;
 
 import org.springframework.stereotype.Service;
 import pl.robertojavadev.carrentalapp.bodystyle.domain.model.BodyStyle;
+import pl.robertojavadev.carrentalapp.bodystyle.domain.repository.BodyStyleRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class BodyStyleService {
+
+    private final BodyStyleRepository bodyStyleRepository;
+
+    public BodyStyleService(BodyStyleRepository bodyStyleRepository) {
+        this.bodyStyleRepository = bodyStyleRepository;
+    }
 
     public List<BodyStyle> getBodyStyles(){
         return null;
