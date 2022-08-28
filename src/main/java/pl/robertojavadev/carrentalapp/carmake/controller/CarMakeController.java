9fac96b393061f3d43027f80ec8b75eecb0f1f08
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-28 08:23:15 +0200

    implemented CarMakeController class

--- src/main/java/pl/robertojavadev/carrentalapp/carmake/controller/CarMakeController.java
+++ src/main/java/pl/robertojavadev/carrentalapp/carmake/controller/CarMakeController.java
@@ -1,4 +1,36 @@
 package pl.robertojavadev.carrentalapp.carmake.controller;
 
+import org.springframework.web.bind.annotation.*;
+import pl.robertojavadev.carrentalapp.carmake.domain.model.CarMake;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/car-makes")
 public class CarMakeController {
+
+    @GetMapping
+    public List<CarMake> getCarMakes(){
+        return null;
+    }
+
+    @GetMapping("{id}")
+    public CarMake getCarMake(@PathVariable UUID id){
+        return null;
+    }
+
+    @PostMapping
+    public CarMake createCarMake(@RequestBody CarMake carMake){
+        return null;
+    }
+
+    @PutMapping("{id}")
+    public CarMake updateCarMake(@PathVariable UUID id, @RequestBody CarMake carMake){
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCarMake(@PathVariable UUID id){
+    }
 }
