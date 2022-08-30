3d96247b24e97dc7221900e5e89777438434b7c1
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-30 08:50:33 +0200

    added http status to BodyStyleController

--- src/main/java/pl/robertojavadev/carrentalapp/bodystyle/controller/BodyStyleController.java
+++ src/main/java/pl/robertojavadev/carrentalapp/bodystyle/controller/BodyStyleController.java
@@ -1,10 +1,12 @@
 package pl.robertojavadev.carrentalapp.bodystyle.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import pl.robertojavadev.carrentalapp.bodystyle.domain.model.BodyStyle;
 import pl.robertojavadev.carrentalapp.bodystyle.service.BodyStyleService;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -23,21 +25,24 @@ public class BodyStyleController {
     }
 
     @GetMapping("{id}")
-    public BodyStyle getBodyStyle(@PathVariable UUID id){
+    public Optional<BodyStyle> getBodyStyle(@PathVariable UUID id){
         return bodyStyleService.getBodyStyle(id);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BodyStyle createBodyStyle(@RequestBody BodyStyle bodyStyle){
         return bodyStyleService.createBodyStyle(bodyStyle);
     }
 
     @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public BodyStyle updateBodyStyle(@PathVariable UUID id, @RequestBody BodyStyle bodyStyle){
         return bodyStyleService.updateBodyStyle(id, bodyStyle);
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBodyStyle(@PathVariable UUID id){
         bodyStyleService.deleteBodyStyle(id);
     }
