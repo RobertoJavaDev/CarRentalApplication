27590c0fa182bf21587a9887c82f56861fadb100
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-31 16:45:00 +0200

    added annotations Transactional to CarMakeService

--- src/main/java/pl/robertojavadev/carrentalapp/carmake/service/CarMakeService.java
+++ src/main/java/pl/robertojavadev/carrentalapp/carmake/service/CarMakeService.java
@@ -1,6 +1,7 @@
 package pl.robertojavadev.carrentalapp.carmake.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.robertojavadev.carrentalapp.carmake.domain.model.CarMake;
 import pl.robertojavadev.carrentalapp.carmake.domain.repository.CarMakeRepository;
 
@@ -17,26 +18,31 @@ public class CarMakeService {
         this.carMakeRepository = carMakeRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<CarMake> getCarMakes(){
         return carMakeRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Optional<CarMake> getCarMake(UUID id){
         return carMakeRepository.findById(id);
     }
 
+    @Transactional
     public CarMake createCarMake(CarMake carMakeRequest){
         CarMake carMake = new CarMake();
         carMake.setName(carMakeRequest.getName());
         return carMakeRepository.save(carMake);
     }
 
+    @Transactional
     public CarMake updateCarMake(UUID id, CarMake carMakeRequest){
         CarMake carMake = carMakeRepository.getById(id);
         carMake.setName(carMakeRequest.getName());
         return carMakeRepository.save(carMake);
     }
 
+    @Transactional
     public void deleteCarMake(UUID id){
         carMakeRepository.deleteById(id);
     }
