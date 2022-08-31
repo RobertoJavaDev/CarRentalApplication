b0ccc63e69892e6ec44636499dfcb0d27dcce3c1
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-31 16:29:42 +0200

    implemented CarMakeRepository

--- src/main/java/pl/robertojavadev/carrentalapp/carmake/domain/repository/CarMakeRepository.java
+++ src/main/java/pl/robertojavadev/carrentalapp/carmake/domain/repository/CarMakeRepository.java
@@ -1,4 +1,11 @@
 package pl.robertojavadev.carrentalapp.carmake.domain.repository;
 
-public interface CarMakeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
+import pl.robertojavadev.carrentalapp.carmake.domain.model.CarMake;
+
+import java.util.UUID;
+
+@RestController
+public interface CarMakeRepository extends JpaRepository <CarMake, UUID> {
 }
