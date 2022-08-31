9d092ccdd6577f62c0c8dbbb3fae579d38d761d4
RobertoJavaDev <robertojavadev@gmail.com>
2022-08-31 16:42:24 +0200

    implemented method in CarMakeService

--- src/main/java/pl/robertojavadev/carrentalapp/carmake/service/CarMakeService.java
+++ src/main/java/pl/robertojavadev/carrentalapp/carmake/service/CarMakeService.java
@@ -2,29 +2,42 @@ package pl.robertojavadev.carrentalapp.carmake.service;
 
 import org.springframework.stereotype.Service;
 import pl.robertojavadev.carrentalapp.carmake.domain.model.CarMake;
+import pl.robertojavadev.carrentalapp.carmake.domain.repository.CarMakeRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class CarMakeService {
 
-    public List<CarMake> getCarMakes(){
-        return null;
+    private final CarMakeRepository carMakeRepository;
+
+    public CarMakeService(CarMakeRepository carMakeRepository) {
+        this.carMakeRepository = carMakeRepository;
     }
 
-    public CarMake getCarMake(UUID id){
-        return null;
+    public List<CarMake> getCarMakes(){
+        return carMakeRepository.findAll();
+    }
+
+    public Optional<CarMake> getCarMake(UUID id){
+        return carMakeRepository.findById(id);
     }
 
     public CarMake createCarMake(CarMake carMakeRequest){
-        return null;
+        CarMake carMake = new CarMake();
+        carMake.setName(carMakeRequest.getName());
+        return carMakeRepository.save(carMake);
     }
 
     public CarMake updateCarMake(UUID id, CarMake carMakeRequest){
-        return null;
+        CarMake carMake = carMakeRepository.getById(id);
+        carMake.setName(carMakeRequest.getName());
+        return carMakeRepository.save(carMake);
     }
 
     public void deleteCarMake(UUID id){
+        carMakeRepository.deleteById(id);
     }
 }
