ef1daa9817d87c6c93816dcd916a1b4e039b9d27
RobertoJavaDev <robertojavadev@gmail.com>
2022-09-01 07:06:39 +0200

    added new method findByCarMakeId to CarRepository

--- src/main/java/pl/robertojavadev/carrentalapp/carmake/domain/repository/CarRepository.java
+++ src/main/java/pl/robertojavadev/carrentalapp/carmake/domain/repository/CarRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.robertojavadev.carrentalapp.carmake.domain.model.Car;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CarRepository extends JpaRepository <Car, UUID> {
+    List<Car> findByCarMakeId(UUID carMakeId);
 }
